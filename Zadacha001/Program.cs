29663bb09a82da838823bf1c03224a99a6a2acf7
Azat Minimullin <minimullin.azat@yandex.ru>
2022-06-25 11:27:00 +0300

    add Zadacha 001

---
+++ Zadacha001/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите программу, которая на вход 
+// принимает два числа и проверяет, 
+// является ли второе число квадратом первого.
+int numberA = 5;
+int numberB = 25;
+if (numberA*numberA == numberB)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
